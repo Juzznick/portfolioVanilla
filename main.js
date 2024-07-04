033867677905ae44d53b77b527dea53f202fd4eb
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-07-04 21:24:56 +0530

    added cards

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 window.addEventListener('scroll', reveal);
-function reveal(){
+function reveal() {
     var reveals = document.querySelectorAll('.reveal');
-    for(var i=0; i<reveals.length;i++){
+    for (var i = 0; i < reveals.length; i++) {
         var windowheight = window.innerHeight;
         var revealtop = reveals[i].getBoundingClientRect().top;
         var revealpoint = 150;
-        if(revealtop < windowheight - revealpoint){
+        if (revealtop < windowheight - revealpoint) {
             reveals[i].classList.add('active');
         }
-        else{
+        else {
             reveals[i].classList.remove('active');
         }
     }
 }
+
+(function () {
+    emailjs.init({
+        publicKey: "DgcDeUInrHpbiN5RV",
+    });
+})();
+
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_4dportfolio";
+    const templateID = "template_mqsbgeb";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
