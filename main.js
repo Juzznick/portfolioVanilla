e58b1004aae8c9f341c2d05f4a7f4d09307dde59
Nikhil <79796884+Juzznick@users.noreply.github.com>
2024-08-01 01:43:29 +0530

    api

--- main.js
+++ main.js
@@ -14,38 +14,9 @@ function reveal() {
     }
 }
 
-
-
-
-var requirejs = require('requirejs');
-
-requirejs.config({
-    //Pass the top-level main.js/index.js require
-    //function to requirejs so that node modules
-    //are loaded relative to the top-level JS file.
-    nodeRequire: require
-});
-
-requirejs(['foo', 'bar'],
-function   (foo,   bar) {
-    //foo and bar are loaded according to requirejs
-    //config, but if not found, then node's require
-    //is used to load the module.
-});
-
-require('dotenv').config();
-
-const publicKey = process.env.EmailjsPublicKey;
-
 (function () {
-    emailjs.init(publicKey);
+    emailjs.init({publicKey: 'DgcDeUInrHpbiN5RV'});
 })();
-
-
-
-
-
-
 
 function sendMail() {
     var params = {
